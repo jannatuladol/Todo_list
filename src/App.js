a37ae1fbca27745846ce5086c9670b1a539cb703
jannatuladol <jannatuladol135@gmail.com>
2024-01-19 10:51:09 +0600

    todo-list

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './index.css';
+import ViewTodoForm from './components/ViewTodoForm';
+
+function App() {
+  return (
+    <>
+      <ViewTodoForm/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/respons/apiResponse.js
@@ -0,0 +1,66 @@
+export const API_TODO_LIST = [
+    {
+        title: 'This is the Title',
+        note: 'Lorem ipsum dolor sit amet. Ut ullam tempore et quia internos et alias velit ut facere vero.',
+        start_date: '14 Sep',
+        end_date: '17 Sep',
+        start_time: '11:00 AM',
+        end_time: '02:00 PM'
+    },
+    {
+        title: 'This is the Title',
+        note: 'Lorem ipsum dolor sit amet. Ut ullam tempore et quia internos et alias velit ut facere vero.',
+        start_date: '14 Sep',
+        end_date: '17 Sep',
+        start_time: '11:00 AM',
+        end_time: '02:00 PM'
+    },
+    {
+        title: 'This is the Title',
+        note: 'Lorem ipsum dolor sit amet. Ut ullam tempore et quia internos et alias velit ut facere vero.',
+        start_date: '14 Sep',
+        end_date: '17 Sep',
+        start_time: '11:00 AM',
+        end_time: '02:00 PM'
+    },
+    {
+        title: 'This is the Title',
+        note: 'Lorem ipsum dolor sit amet. Ut ullam tempore et quia internos et alias velit ut facere vero.',
+        start_date: '14 Sep',
+        end_date: '17 Sep',
+        start_time: '11:00 AM',
+        end_time: '02:00 PM'
+    },
+    {
+        title: 'This is the Title3',
+        note: 'Lorem ipsum dolor sit amet. Ut ullam tempore et quia internos et alias velit ut facere vero.',
+        start_date: '14 Sep',
+        end_date: '17 Sep',
+        start_time: '11:00 AM',
+        end_time: '02:00 PM'
+    },
+    {
+        title: 'This is the Title3',
+        note: 'Lorem ipsum dolor sit amet. Ut ullam tempore et quia internos et alias velit ut facere vero.',
+        start_date: '14 Sep',
+        end_date: '17 Sep',
+        start_time: '11:00 AM',
+        end_time: '02:00 PM'
+    },
+    {
+        title: 'This is the Title3',
+        note: 'Lorem ipsum dolor sit amet. Ut ullam tempore et quia internos et alias velit ut facere vero.',
+        start_date: '14 Sep',
+        end_date: '17 Sep',
+        start_time: '11:00 AM',
+        end_time: '02:00 PM'
+    },
+    {
+        title: 'This is the Title3',
+        note: 'Lorem ipsum dolor sit amet. Ut ullam tempore et quia internos et alias velit ut facere vero.',
+        start_date: '14 Sep',
+        end_date: '17 Sep',
+        start_time: '11:00 AM',
+        end_time: '02:00 PM'
+    },
+]
